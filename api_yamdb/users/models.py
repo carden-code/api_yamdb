18fcfb322e6971128b80d59f4dba051c92ed99d7
Vyacheslav Borisenko <borisenkov89@yandex.ru>
2022-03-29 21:46:43 +0300

    modification custom user

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,53 +1,41 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class MyUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None):
-        user = self.create_user(
-            username=username,
-            email=email,
-            password=password,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-
 class User(AbstractUser):
-    ROLES = (("user", "USER"), ("moderator", "MODERATOR"), ("admin", "ADMIN"))
+    ROLES = (('user', 'USER'), ('moderator', 'MODERATOR'), ('admin', 'ADMIN'))
 
-    email = models.EmailField(max_length=254, unique=True, blank=False)
-    bio = models.TextField(verbose_name="Биография", blank=True)
-    role = models.CharField(max_length=300, choices=ROLES, default=ROLES[0][0])
-
-    objects = MyUserManager()
-
-    @property
-    def is_admin(self):
-        return self.is_superuser or self.role == "admin"
-
-    @property
-    def is_moderator(self):
-        return self.role == "moderator"
-
-    def __str__(self):
-        return self.username
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        max_length=254,
+        unique=True,
+        blank=False
+    )
+    password = models.CharField(
+        verbose_name='Пароль',
+        max_length=128,
+        blank=True
+    )
+    bio = models.TextField(verbose_name='Биография', blank=True)
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=300,
+        choices=ROLES,
+        default=ROLES[0][0]
+    )
 
     class Meta:
         ordering = ['-id']
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    @property
+    def is_admin(self):
+        return self.is_superuser or self.role == 'admin'
+
+    @property
+    def is_moderator(self):
+        return self.role == 'moderator'
+
+    def __str__(self):
+        return self.username
