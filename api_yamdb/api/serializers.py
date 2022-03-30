e648fbce07855d827da05110239966f685b0a873
“Елена <borovina@bk.ru>
2022-03-30 17:47:41 +0300

    tra-ta-ta

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from reviews.models import Title, Category, Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Genre
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    rating = serializers.IntegerField(read_only=True)
+    genre = GenreSerializer(many=True, read_only=True)
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Title
+
+
+class TitleWriteSerializer(serializers.ModelSerializer):
+    rating = serializers.IntegerField(read_only=True)
+    genre = serializers.SlugRelatedField(
+        many=True,
+        slug_field='slug',
+        queryset=Genre.objects.all()
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Title
+
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, verbose_name='name')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='индентификатор')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'категория'
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256, verbose_name='имя')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='индентификатор')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'жанр'
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=30, unique=True, verbose_name='имя')
+    description = models.TextField(verbose_name='описание')
+    year = models.IntegerField()
+    genre = models.ManyToManyField(
+        'Genre',
+        related_name='genre'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='category',
+        null=True,
+        verbose_name='категория'
+    )
+
+    class Meta:
+        ordering = ('name', 'year',)
+        verbose_name = 'произведения'
+
