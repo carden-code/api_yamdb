7154dfde19180bca8102ff4f6791dfcd59554480
“Елена <borovina@bk.ru>
2022-04-02 13:39:52 +0300

    «10»

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from audioop import avg
+from distutils.util import Mixin2to3
+from warnings import filters
+from reviews.models import Category, Genre, Title
 
-# Create your views here.
+from .serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleReadSerializer,
+    TitleWriteSerializer,
+)
+
+from .filters import TitleFilter
+
+class CategoryViewSet(
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (ReadOnlyPermission | IsAdminPermission,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_fields = ('name',)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (ReadOnlyPermission | IsAdminPermission,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_fields = ('name',)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all().annotate(rating=avg('reviews__score'))
+    permission_classes = (ReadOnlyPermission | IsAdminPermission,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = (
+        'category',
+        'genre',
+        'name',
+        'year',
+    )
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' or self.action == 'list':
+            return TitleReadSerializer
+        return TitleWriteSerializer
